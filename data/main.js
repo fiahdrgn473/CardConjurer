77f03828e7acd62a0623c31b383b07c214f3094a
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2019-04-21 17:18:00 -0700

    Update main.js

--- data/main.js
+++ data/main.js
@@ -820,6 +820,7 @@ function toggleView(targetId, targetClass) {
 
 //Downloads the image!
 function downloadCardImage(linkElement) {
+	linkElement.download = document.getElementById("inputName").value + ".png"
 	var cardImageData = cardCanvas.toDataURL()
 	if (cardImageData == undefined) {
 		alert("Sorry, it seems that you cannot download your card. Please try using a different browser/device.")
