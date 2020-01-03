12003775339041a163df06d660f89c460542673c
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2020-01-02 17:38:54 -0800

    pinline

--- data/versions/m15extras.js
+++ data/versions/m15extras.js
@@ -4,12 +4,12 @@
 if (version.currentVersion != "m15") {
     //Name, text, x, y, width, height, font, size, color, other
     version.textList = [
-                        ["Title", "", scale(64), scale(83), scale(630), 0, "belerenb", 37, "black", "oneLine=true"],
-                        ["Type", "", scale(64), scale(615), scale(630), 0, "belerenb", 33, "black", "oneLine=true"],
-                        ["Rules Text", "", scale(64), scale(662), scale(616), scale(292), "mplantin", 38, "black", "lineSpace=0.97"],
-                        ["Power Toughness", "", scale(587), scale(958), scale(110), 0, "belerenb", 38, "black", "oneLine=true,textAlign='center'"]/*,
-                                                                                                                                                   ["Flip PT", "", scale(588), scale(902), scale(100), 0, "belerenb", 28, "#666", "oneLine=true,textAlign='right'"]*/
-                        ]
+        ["Title", "", scale(64), scale(83), scale(630), 0, "belerenb", 37, "black", "oneLine=true"],
+        ["Type", "", scale(64), scale(615), scale(630), 0, "belerenb", 33, "black", "oneLine=true"],
+        ["Rules Text", "", scale(64), scale(662), scale(616), scale(292), "mplantin", 38, "black", "lineSpace=0.97"],
+        ["Power Toughness", "", scale(587), scale(958), scale(110), 0, "belerenb", 38, "black", "oneLine=true,textAlign='center'"]/*,
+        ["Flip PT", "", scale(588), scale(902), scale(100), 0, "belerenb", 28, "#666", "oneLine=true,textAlign='right'"]*/
+        ]
 }
 version.currentVersion = "m15"
 version.artX = scale(58)
@@ -29,7 +29,7 @@ version.manaCostDirection = "horizontal"
 version.watermarkWidth = scale(520)
 version.watermarkHeight = scale(250)
 version.watermarkY = scale(800)
-version.masksToAdd = []
+version.masksToAdd = ["Pinline Alternate"]
 filterFramePicker("frameClassRegular");
 m15BottomInfo()
 finishChangingVersion("data/images/m15Extras/m15ExtrasCSV.csv")
