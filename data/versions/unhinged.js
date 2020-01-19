5066f16ec2478dec8e18ba491ea3250e285ba5c4
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2020-01-19 09:06:18 -0800

    Update unhinged.js

--- data/versions/unhinged.js
+++ data/versions/unhinged.js
@@ -33,7 +33,7 @@ filterFramePicker("frameClassUnhinged")
 
 function unhingedBottomInfo() {
     bottomInfoContext.clearRect(0, 0, cardWidth, cardHeight)
-    bottomInfoContext.writeText("{oldArtistBrush}John Avon" + document.getElementById("inputInfoArtist").value, scale(60), scale(964), scale(624), 0, "matrixb", scale(26), 'white', "oneLine=true")
+    bottomInfoContext.writeText("{oldArtistBrush}" + document.getElementById("inputInfoArtist").value, scale(60), scale(964), scale(624), 0, "matrixb", scale(26), 'white', "oneLine=true")
     bottomInfoContext.writeText("\u2122 & \u00a9 " + date.getFullYear() + " Wizards of the Coast *Not For Sale*", scale(62), scale(987), scale(620), 0, "mplantin", scale(17), 'white', "oneLine=true")
     cardImageUpdated()
 }
