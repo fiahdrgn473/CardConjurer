fbd4986be7f7128d37553df56ecd9f85cb00655a
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2020-05-27 15:53:34 -0700

    Update inventionClassic.js

--- data/scripts/versions/m15/inventionClassic.js
+++ data/scripts/versions/m15/inventionClassic.js
@@ -1,6 +1,6 @@
 if (!loadedVersions.includes('m15InventionClassic')) {
 	loadedVersions.push("m15InventionClassic")
-	loadMaskImages([['Frame Simple Invention (m15)', 'data/images/m15/inventionClassic/m15MaskInventionClassicFrame.png'])
+	loadMaskImages([['Frame Simple Invention (m15)', 'data/images/m15/inventionClassic/m15MaskInventionClassicFrame.png']])
 	loadFrameImages([
 		['White Invention Frame', 'data/images/m15/inventionClassic/m15FrameWInventionClassic.png', 0, 0, 1, 1, ['Full', 'Pinline (m15)', 'Title (m15)', 'Type (m15)', 'Rules (m15)', 'Frame (m15)', 'Border (m15)', 'Pinline Super (m15)', 'Frame Simple Invention (m15)']],
 		['Blue Invention Frame', 'data/images/m15/inventionClassic/m15FrameUInventionClassic.png', 0, 0, 1, 1, ['Full', 'Pinline (m15)', 'Title (m15)', 'Type (m15)', 'Rules (m15)', 'Frame (m15)', 'Border (m15)', 'Pinline Super (m15)', 'Frame Simple Invention (m15)']],
