1e6f092cc8ce5cfb7a90e72a1a236de4458c1479
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2020-05-26 17:28:00 -0700

    inventionClassic

---
+++ data/scripts/versions/m15/inventionClassic.js
@@ -0,0 +1,14 @@
+if (!loadedVersions.includes('m15InventionClassic')) {
+	loadedVersions.push("m15InventionClassic")
+	loadFrameImages([
+		['White Invention Frame', 'data/images/m15/inventionClassic/m15FrameWInventionClassic.png', 0, 0, 1, 1, ['Full', 'Pinline (m15)', 'Title (m15)', 'Type (m15)', 'Rules (m15)', 'Frame (m15)', 'Border (m15)', 'Pinline Super (m15)']],
+		['Blue Invention Frame', 'data/images/m15/inventionClassic/m15FrameUInventionClassic.png', 0, 0, 1, 1, ['Full', 'Pinline (m15)', 'Title (m15)', 'Type (m15)', 'Rules (m15)', 'Frame (m15)', 'Border (m15)', 'Pinline Super (m15)']],
+		['Black Invention Frame', 'data/images/m15/inventionClassic/m15FrameBInventionClassic.png', 0, 0, 1, 1, ['Full', 'Pinline (m15)', 'Title (m15)', 'Type (m15)', 'Rules (m15)', 'Frame (m15)', 'Border (m15)', 'Pinline Super (m15)']],
+		['Red Invention Frame', 'data/images/m15/inventionClassic/m15FrameRInventionClassic.png', 0, 0, 1, 1, ['Full', 'Pinline (m15)', 'Title (m15)', 'Type (m15)', 'Rules (m15)', 'Frame (m15)', 'Border (m15)', 'Pinline Super (m15)']],
+		['Green Invention Frame', 'data/images/m15/inventionClassic/m15FrameGInventionClassic.png', 0, 0, 1, 1, ['Full', 'Pinline (m15)', 'Title (m15)', 'Type (m15)', 'Rules (m15)', 'Frame (m15)', 'Border (m15)', 'Pinline Super (m15)']],
+		['Multicolored Invention Frame', 'data/images/m15/inventionClassic/m15FrameMInventionClassic.png', 0, 0, 1, 1, ['Full', 'Pinline (m15)', 'Title (m15)', 'Type (m15)', 'Rules (m15)', 'Frame (m15)', 'Border (m15)', 'Pinline Super (m15)']],
+		['Artifact Invention Frame', 'data/images/m15/inventionClassic/m15FrameAInventionClassic.png', 0, 0, 1, 1, ['Full', 'Pinline (m15)', 'Title (m15)', 'Type (m15)', 'Rules (m15)', 'Frame (m15)', 'Border (m15)', 'Pinline Super (m15)']]
+	], 'frameClassM15InventionClassic')
+}
+
+hideFrameImages('frameClassM15InventionClassic')
