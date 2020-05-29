60f6d56fa8d81654b973b977e8b003ecfd555a91
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2020-05-28 20:19:02 -0700

    seventh textless land

--- data/scripts/versions/seventhTextless/regular.js
+++ data/scripts/versions/seventhTextless/regular.js
@@ -8,6 +8,7 @@ if (!loadedVersions.includes('seventhTextlessRegular')) {
 		['Green Frame', 'data/images/seventhTextless/seventhTextlessFrameG.png', 0, 0, 1, 1, ['Full', 'Pinline (seventh textless)']],
 		['Multicolored Frame', 'data/images/seventhTextless/seventhTextlessFrameM.png', 0, 0, 1, 1, ['Full', 'Pinline (seventh textless)']],
 		['Artifact Frame', 'data/images/seventhTextless/seventhTextlessFrameA.png', 0, 0, 1, 1, ['Full', 'Pinline (seventh textless)']],
+		['Land Frame', 'data/images/seventhTextless/seventhTextlessFrameL.png', 0, 0, 1, 1, ['Full', 'Pinline (seventh textless)']],
 		['Foil Stamp', 'data/images/seventh/seventhFoilStamp.png', 81/744, 867/1039, 300/744, 115/1039, ['Full', 'Pinline (seventh textless)']]
 	], 'frameClassSeventhTextlessRegular')
 }
