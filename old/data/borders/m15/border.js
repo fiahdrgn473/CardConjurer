4043bc874d0e403bb91c8f93c7c2b184f695d00c
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2020-07-06 16:14:29 -0700

    cleanup

--- old/data/borders/m15/border.js
+++
@@ -1,7 +0,0 @@
-//============================================//
-//                 M15 Border                 //
-//============================================//
-//Loads the colors
-loadColors("white-White,blue-Blue,black-Black,red-Red,green-Green,gold-Gold,artifact-Artifact,colorless-Colorless,vehicle-Vehicle,whiteLand-White Land,blueLand-Blue Land,blackLand-Black Land,redLand-Red Land,greenLand-Green Land,goldLand-Gold Land,colorlessLand-Colorless Land")
-//Finishes loading the border style
-finishChangingBorder()
