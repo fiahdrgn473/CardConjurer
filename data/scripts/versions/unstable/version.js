f2902664df3781c1bb28406a1e5d684cc7812384
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2020-07-15 14:41:57 -0700

    unstable basics

---
+++ data/scripts/versions/unstable/version.js
@@ -0,0 +1,39 @@
+if (!loadedVersions.includes('unstable')) {
+	loadedVersions.push('unstable')
+	// loadMaskImages([])
+}
+
+if (currentVersion != 'unstable') {
+	currentVersion = 'unstable'
+
+	loadTextOptions([
+	new cardText('Card Title', '', 165/1500, 90/2100, 1170/1500, 80/2100, 'belerenb', 80/2100, 'white', ['oneLine=true','textAlign="center"']),
+	])
+
+	artX = 0
+	artY = 0
+	artWidth = scaleX(1)
+	artHeight = scaleY(1931/2100)
+
+	manaCostXPath = '0'
+	manaCostYPath = '0'
+	manaCostDiameter = '0'
+	manaCostShadowOffset = 'none'
+	manaCostDirection = 'reverse'
+
+	setSymbolX = [cardWidth / 2, 'center']
+	setSymbolY = [scaleY(244/2100), 'center']
+	setSymbolWidth = 0
+	setSymbolHeight = 0
+
+	watermarkX = 0
+	watermarkY = 0
+	watermarkWidth = 0
+	watermarkHeight = 0
+
+	bottomInfoFunction = 'bottomInfoM15'
+
+	bottomInfoUpdated()
+}
+
+loadFramePackOptions([['regular', 'Regular']])
