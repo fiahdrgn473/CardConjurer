ad638ef47d58ca67f533a9256c9badc2bde549c2
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2020-12-16 17:09:56 -0800

    php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include('globalHTML/header.php'); ?>
+	<title>Card Conjurer</title>
+	<div class='layer center'>
+		<h2>Welcome to Card Conjurer</h2>
+	</div>
+	<div class='layer center'>
+		<h2>The home page is currently under construction...</h2>
+	</div>
+<?php include('globalHTML/footer.php'); ?>
