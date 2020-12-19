e6d4777f230b67b2cee4479f2b272a1a38ec7031
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2020-12-19 11:32:13 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function toggleMenu() {
 		document.querySelector('.hamburger').classList.remove('opened');
 		Array.from(document.querySelectorAll('.menu-visible')).forEach(element => element.classList.remove('menu-visible'));
 	} else {
-		document.documentElement.style.setProperty('--window-diagonal-size', (Math.floor(Math.sqrt(window.innerWidth ** 2 + window.innerHeight ** 2)) + 2) + 'px');
+		document.documentElement.style.setProperty('--window-diagonal-size', (Math.floor(Math.sqrt(window.innerWidth ** 2 + window.innerHeight ** 2)) + 100) + 'px');
 		document.querySelector('.hamburger').classList.add('opened');
 		document.querySelector('.menu').classList.add('menu-visible');
 	}
