bd04f9aa1660b23038f41ba4db986f2e25233b68
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2020-12-20 15:02:06 -0800

    Update creator.js

--- js/creator.js
+++ js/creator.js
@@ -150,7 +150,7 @@ function dragEnd(event) {
 	Array.from(document.querySelectorAll('.dragging')).forEach(element => element.classList.remove('dragging'));
 }
 function touchMove(event) {
-	// event.preventDefault();
+	event.preventDefault();
 	var clientX = event.touches[0].clientX;
 	var clientY = event.touches[0].clientY;
 	Array.from(document.querySelector('.dragging').parentElement.children).forEach(element => {
