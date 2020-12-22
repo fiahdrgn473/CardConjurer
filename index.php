6683a6bcca4d9687c8980e99e8c7b6e03bc17588
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2020-12-22 10:24:40 -0800

    styling

--- index.php
+++ index.php
@@ -3,23 +3,23 @@
 	<div class='layer center'></div>
 	<div class='layer center'>
 		<h1>Welcome to Card Conjurer</h1>
-		<h4>The custom Magic: The Gathering card creator</h4>
+		<h3>The custom Magic: The Gathering card creator</h3>
 	</div>
 	<div class='layer center'></div>
 	<div class='layer readable-background center'>
 		<div class='sample-grid'>
 			<img src='/img/samples/sample1.png'>
 			<div class='vertical-center'>
-				<h2 class='padding margin-bottom'>Choose from a Variety of Card Frames</h2>
-				<h4 class='padding margin-bottom'>Card Conjurer offers Expeditions, Inventions, Showcase Frames, and so much more. <a href='/creator/'>Take a look</a>!</h4>
+				<h1 class='padding margin-bottom'>Choose from a Variety of Card Frames</h1>
+				<h3 class='padding margin-bottom'>Card Conjurer offers Expeditions, Inventions, Showcase Frames, and so much more. <a href='/creator/'>Take a look</a>!</h3>
 			</div>
 		</div>
 	</div>
 	<div class='layer center'>
 		<div class='sample-grid right'>
 			<div class='vertical-center'>
-				<h2 class='padding margin-bottom'>Customize to Your Heart's Content</h2>
-				<h4 class='padding margin-bottom'>With Card Conjurer's level of customization, you'll have endless options when designing your dream cards. <a href='/creator/'>Try it out</a>!</h4>
+				<h1 class='padding margin-bottom'>Customize to Your Heart's Content</h1>
+				<h3 class='padding margin-bottom'>With Card Conjurer's level of customization, you'll have endless options when designing your dream cards. <a href='/creator/'>Try it out</a>!</h3>
 			</div>
 			<img src='/img/samples/sample2.png'>
 		</div>
@@ -28,13 +28,13 @@
 		<div class='sample-grid'>
 			<img src='/img/samples/sample3.png'>
 			<div class='vertical-center'>
-				<h2 class='padding margin-bottom'>Bling Out Existing Favorites</h2>
-				<h4 class='padding margin-bottom'>Easily import vital information from existing cards, then redesign them. <a href='/creator/'>Go on, do it</a>!</h4>
+				<h1 class='padding margin-bottom'>Bling Out Existing Favorites</h1>
+				<h3 class='padding margin-bottom'>Easily import vital information from existing cards, then redesign them. <a href='/creator/'>Go on, do it</a>!</h3>
 			</div>
 		</div>
 	</div>
 	<div class='layer center'>
-		<h2 class='margin-bottom'>Ready?</h2>
+		<h1 class='margin-bottom'>Ready?</h1>
 		<a href='/creator/'><h1>Get Started</h1></a>
 	</div>
 <?php include('globalHTML/footer.php'); ?>
