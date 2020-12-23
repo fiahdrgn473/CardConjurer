b49ba364504eb0bbc901a9c20e71809d1923dd29
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2020-12-23 12:12:30 -0800

    animations

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<div class='layer center'></div>
 	<div class='layer readable-background center'>
 		<div class='sample-grid'>
-			<img src='/img/samples/sample1.png'>
+			<div class='animated-scene'><img src='/img/samples/sample1.png' class='animated-card-1'></div>
 			<div class='vertical-center'>
 				<h1 class='padding margin-bottom'>Choose from a Variety of Card Frames</h1>
 				<h3 class='padding margin-bottom'>Card Conjurer offers Expeditions, Inventions, Showcase Frames, and so much more. <a href='/creator/'>Take a look</a>!</h3>
@@ -21,12 +21,12 @@
 				<h1 class='padding margin-bottom'>Customize to Your Heart's Content</h1>
 				<h3 class='padding margin-bottom'>With Card Conjurer's level of customization, you'll have endless options when designing your dream cards. <a href='/creator/'>Try it out</a>!</h3>
 			</div>
-			<img src='/img/samples/sample2.png'>
+			<div class='animated-scene'><img src='/img/samples/sample2.png' class='animated-card-1 animation-delay-2'></div>
 		</div>
 	</div>
 	<div class='layer readable-background center'>
 		<div class='sample-grid'>
-			<img src='/img/samples/sample3.png'>
+			<div class='animated-scene'><img src='/img/samples/sample3.png' class='animated-card-1 animation-delay-4'></div>
 			<div class='vertical-center'>
 				<h1 class='padding margin-bottom'>Bling Out Existing Favorites</h1>
 				<h3 class='padding margin-bottom'>Easily import vital information from existing cards, then redesign them. <a href='/creator/'>Go on, do it</a>!</h3>
