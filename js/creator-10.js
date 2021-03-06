eacfd24a43a7dacfbcedfbfe7f3205c3ef7d6983
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2021-03-06 11:30:23 -0800

    Update creator-10.js

--- js/creator-10.js
+++ js/creator-10.js
@@ -215,10 +215,10 @@ function toggleCollapse(event) {
 }
 //Mana Symbols
 var manaSymbols = [];
-loadManaSymbols(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', 'w', 'u', 'b', 'r', 'g', 'c', 'x', 'y', 'z', 't', 'untap', 'e', 's', 'chaos']);
+loadManaSymbols(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', 'w', 'u', 'b', 'r', 'g', 'c', 'x', 'y', 'z', 't', 'untap', 'e', 's']);
 loadManaSymbols(['wu', 'wb', 'ub', 'ur', 'br', 'bg', 'rg', 'rw', 'gw', 'gu', '2w', '2u', '2b', '2r', '2g', 'wp', 'up', 'bp', 'rp', 'gp'], [1.2, 1.2]);
 loadManaSymbols(['bar.png', 'whitebar.png']);
-loadManaSymbols(['chaos'], [1, 10.2]);
+loadManaSymbols(['chaos'], [1.2, 1]);
 function loadManaSymbols(manaSymbolPaths, size = [1, 1]) {
 	manaSymbolPaths.forEach(item => {
 		var manaSymbol = {};
