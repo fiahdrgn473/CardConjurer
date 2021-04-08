d9d641efd6203d664c60064a1226e85a97567f50
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2021-04-08 14:05:58 -0700

    Update groupPlaneswalker.js

--- js/frames/groupPlaneswalker.js
+++ js/frames/groupPlaneswalker.js
@@ -2,6 +2,7 @@ loadFramePacks([
 	{name:'Regular Frames', value:'PlaneswalkerRegular'},
 	{name:'Borderless Frames', value:'PlaneswalkerBorderless'},
 	{name:'Nickname Frames', value:'PlaneswalkerNickname'},
-	{name:'Tall Frames', value:'PlaneswalkerTall'}
+	{name:'Tall Frames', value:'PlaneswalkerTall'},
+	{name:'MDFC Frames', value:'PlaneswalkerMDFC'}
 ])
 notify('When you load a Planeswalker frame version, a "Planeswalker" tab will appear. This tab controls the placement and loyalty costs for Planeswalker abilities.')
