acc9dcaf2e4f0068442decea6ebde21c7fe79963
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2021-04-12 15:03:39 -0700

    MDFC Floating crowns

--- js/frames/groupModal-1.js
+++ js/frames/groupModal-1.js
@@ -3,7 +3,7 @@ loadFramePacks([
 	{name:'Nickname', value:'ModalShortNickname'},
 	{name:'Addons', value:'disabled'},
 	{name:'Legend Crowns', value:'M15LegendCrowns'},
-	{name:'Floating Legend Crowns', value:'M15LegendCrownsFloating'},
+	{name:'Floating Legend Crowns', value:'ModalLegendCrownsFloating'},
 	{name:'Inner Crowns', value:'M15InnerCrowns'},
 	{name:'Holo Stamps', value:'M15HoloStamps'},
 	{name:'Dark Power/Toughness', value:'M15DarkPT'},
