a566f1252e29213db18e7d6c5ff918fd8d66fd3c
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2021-04-23 17:07:57 -0700

    Feuer Ameise's Ixalan

--- js/frames/groupCustom.js
+++ js/frames/groupCustom.js
@@ -1,6 +1,7 @@
 loadFramePacks([
 	{name:'Cartoony - Sheepwave', value:'Cartoony'},
 	{name:'Classic-Shifted', value:'Classicshifted'},
+	{name:'Ixalan - @feuer_ameise', value:'FeuerAmeiseIxalan'},
 	{name:'Custom Addons', value:'disabled'},
 	{name:'Brawl Legend Crowns', value:'Brawl'}
 ])
