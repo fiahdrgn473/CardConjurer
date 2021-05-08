ce841df329907f6fba8ffa2cf32c447bde7b2c73
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2021-05-07 17:43:20 -0700

    Update packPromoExtended.js

--- js/frames/packPromoExtended.js
+++ js/frames/packPromoExtended.js
@@ -11,7 +11,7 @@ availableFrames = [
 	{name:'Multicolored Frame', src:'/img/frames/promo/extended/m.png', masks:masks},
 	{name:'Artifact Frame', src:'/img/frames/promo/extended/a.png', masks:masks},
 	{name:'Land Frame', src:'/img/frames/promo/extended/l.png', masks:masks},
-	{name:'Colorless Frame', src:'/img/frames/promo/extended/l.png', masks:masks},
+	{name:'Colorless Frame', src:'/img/frames/promo/extended/c.png', masks:masks},
 	{name:'Vehicle Frame', src:'/img/frames/promo/extended/v.png', masks:masks},
 	{name:'White Power/Toughness', src:'/img/frames/m15/regular/m15PTW.png', bounds:bounds},
 	{name:'Blue Power/Toughness', src:'/img/frames/m15/regular/m15PTU.png', bounds:bounds},
