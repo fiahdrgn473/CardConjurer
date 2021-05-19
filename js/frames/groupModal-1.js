6652030049b1feb12ff2fad8938e85a18514c3dd
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2021-05-19 10:09:26 -0700

    MDFC Helper

--- js/frames/groupModal-1.js
+++ js/frames/groupModal-1.js
@@ -10,5 +10,7 @@ loadFramePacks([
 	{name:'Inner Crowns', value:'M15InnerCrowns'},
 	{name:'Holo Stamps', value:'M15HoloStamps'},
 	{name:'Dark Power/Toughness', value:'M15DarkPT'},
-	{name:'Colored Borders', value:'M15Borders'}
+	{name:'Colored Borders', value:'M15Borders'},
+	{name:'Misc', value:'disabled'},
+	{name:'Helper Cards', value:'ModalHelper'}
 ])
