bf56af81377bfdc2d7afacbdbcbc5e981ddfe99e
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2021-05-22 12:45:00 -0700

    Jumpstart Front Card

--- js/frames/groupToken-2.js
+++ js/frames/groupToken-2.js
@@ -2,6 +2,7 @@ loadFramePacks([
 	{name:'Regular', value:'TokenRegular-1'},
 	{name:'Textless', value:'TokenTextless-1'},
 	{name:'Tall', value:'TokenTall-1'},
+	{name:'Jumpstart Front Cards', value:'JMPFront'},
 	{name:'Addons', value:'disabled'},
 	{name:'Floating Legend Crowns', value:'M15LegendCrownsFloating'},
 	{name:'Inner Crowns', value:'M15InnerCrowns'},
