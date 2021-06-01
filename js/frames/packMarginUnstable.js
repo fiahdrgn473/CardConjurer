c7a708e1b717f60ec498924b0434588e2ad146fd
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2021-06-01 09:20:53 -0700

    various fixes

--- js/frames/packMarginUnstable.js
+++ js/frames/packMarginUnstable.js
@@ -1,14 +1,15 @@
 //Create objects for common properties across available frames
 var bounds = {x:-0.044, y:-1/35, width:1.088, height:37/35};
+var ogBounds = {x:0, y:0, width:1, height:1};
 //defines available frames
 availableFrames = [
-	{name:'White Extension', src:'/img/frames/unstable/margins/w.png', bounds:bounds},
-	{name:'Blue Extension', src:'/img/frames/unstable/margins/u.png', bounds:bounds},
-	{name:'Black Extension', src:'/img/frames/unstable/margins/b.png', bounds:bounds},
-	{name:'Red Extension', src:'/img/frames/unstable/margins/r.png', bounds:bounds},
-	{name:'Green Extension', src:'/img/frames/unstable/margins/g.png', bounds:bounds},
-	{name:'Multicolored Extension', src:'/img/frames/unstable/margins/m.png', bounds:bounds},
-	{name:'Land Extension', src:'/img/frames/unstable/margins/c.png', bounds:bounds}
+	{name:'White Extension', src:'/img/frames/unstable/margins/w.png', bounds:bounds, ogBounds:ogBounds},
+	{name:'Blue Extension', src:'/img/frames/unstable/margins/u.png', bounds:bounds, ogBounds:ogBounds},
+	{name:'Black Extension', src:'/img/frames/unstable/margins/b.png', bounds:bounds, ogBounds:ogBounds},
+	{name:'Red Extension', src:'/img/frames/unstable/margins/r.png', bounds:bounds, ogBounds:ogBounds},
+	{name:'Green Extension', src:'/img/frames/unstable/margins/g.png', bounds:bounds, ogBounds:ogBounds},
+	{name:'Multicolored Extension', src:'/img/frames/unstable/margins/m.png', bounds:bounds, ogBounds:ogBounds},
+	{name:'Land Extension', src:'/img/frames/unstable/margins/c.png', bounds:bounds, ogBounds:ogBounds}
 ];
 //disables/enables the "Load Frame Version" button
 document.querySelector('#loadFrameVersion').disabled = false;
--- js/frames/packModalLegendCrownsBrawl.js
+++ js/frames/packModalLegendCrownsBrawl.js
@@ -1,17 +1,18 @@
 //Create objects for common properties across available frames
-var masks = [{src:'/img/frames/custom/brawl/center.svg', name:'Center'}, {src:'/img/frames/custom/brawl/second.svg', name:'Second'}, {src:'/img/frames/custom/brawl/third.svg', name:'Third'}, {src:'/img/frames/custom/brawl/wings.svg', name:'Wings'}];
-var bounds = {x:0.0094, y:0.0005, width:0.9814, height:0.1262};
+var masks = [{src:'/img/frames/custom/brawl/floating.svg', name:'Crown Without Pinlines'}, {src:'/img/frames/custom/brawl/center.svg', name:'Center'}, {src:'/img/frames/custom/brawl/second.svg', name:'Second'}, {src:'/img/frames/custom/brawl/third.svg', name:'Third'}, {src:'/img/frames/custom/brawl/wings.svg', name:'Wings'}];
+var bounds = {x:0.0087, y:0.0005, width:0.9827, height:0.1853};
 //defines available frames
 availableFrames = [
-	{name:'White Crown', src:'/img/frames/modal/crowns/brawl/w.png', masks:masks, bounds:bounds, complementary:8},
-	{name:'Blue Crown', src:'/img/frames/modal/crowns/brawl/u.png', masks:masks, bounds:bounds, complementary:8},
-	{name:'Black Crown', src:'/img/frames/modal/crowns/brawl/b.png', masks:masks, bounds:bounds, complementary:8},
-	{name:'Red Crown', src:'/img/frames/modal/crowns/brawl/r.png', masks:masks, bounds:bounds, complementary:8},
-	{name:'Green Crown', src:'/img/frames/modal/crowns/brawl/g.png', masks:masks, bounds:bounds, complementary:8},
-	{name:'Multicolored Crown', src:'/img/frames/modal/crowns/brawl/m.png', masks:masks, bounds:bounds, complementary:8},
-	{name:'Artifact Crown', src:'/img/frames/modal/crowns/brawl/a.png', masks:masks, bounds:bounds, complementary:8},
-	{name:'Land Crown', src:'/img/frames/modal/crowns/brawl/l.png', masks:masks, bounds:bounds, complementary:8},
-	{name:'Legend Crown Cutout', src:'/img/frames/modal/crowns/cutout.svg', erase:true}
+	{name:'White Crown', src:'/img/frames/modal/crowns/brawl/w.png', masks:masks, bounds:bounds, complementary:9},
+	{name:'Blue Crown', src:'/img/frames/modal/crowns/brawl/u.png', masks:masks, bounds:bounds, complementary:9},
+	{name:'Black Crown', src:'/img/frames/modal/crowns/brawl/b.png', masks:masks, bounds:bounds, complementary:9},
+	{name:'Red Crown', src:'/img/frames/modal/crowns/brawl/r.png', masks:masks, bounds:bounds, complementary:9},
+	{name:'Green Crown', src:'/img/frames/modal/crowns/brawl/g.png', masks:masks, bounds:bounds, complementary:9},
+	{name:'Multicolored Crown', src:'/img/frames/modal/crowns/brawl/m.png', masks:masks, bounds:bounds, complementary:9},
+	{name:'Artifact Crown', src:'/img/frames/modal/crowns/brawl/a.png', masks:masks, bounds:bounds, complementary:9},
+	{name:'Land Crown', src:'/img/frames/modal/crowns/brawl/l.png', masks:masks, bounds:bounds, complementary:9},
+	{name:'Legend Crown Cutout', src:'/img/frames/modal/crowns/cutout.svg', erase:true},
+	{name:'Legend Crown Border Cover', src:'/img/frames/modal/crowns/cover.svg'}
 ];
 //disables/enables the "Load Frame Version" button
 document.querySelector('#loadFrameVersion').disabled = true;
