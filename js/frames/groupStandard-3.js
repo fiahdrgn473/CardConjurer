086746ece982a5e020421c1e895a01ba6ed6b7f3
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2021-07-19 14:10:36 -0700

    miracles

--- js/frames/groupStandard-3.js
+++ js/frames/groupStandard-3.js
@@ -4,6 +4,7 @@ loadFramePacks([
 	{name:'Legend Crowns', value:'M15LegendCrowns'},
 	{name:'Floating Legend Crowns', value:'M15LegendCrownsFloating'},
 	{name:'Inner Crowns', value:'M15InnerCrowns'},
+	{name:'Miracle', value:'M15Miracle'},
 	{name:'Holo Stamps', value:'M15HoloStamps'},
 	{name:'Dark Power/Toughness', value:'M15DarkPT'},
 	{name:'Colored Borders', value:'M15Borders'},
