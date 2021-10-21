7c0dcfba1e4c408a6f41bb7c12a0f7bb0428219b
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2021-10-21 11:39:52 -0700

    4e

--- js/frames/groupMisc-2.js
+++ js/frames/groupMisc-2.js
@@ -3,6 +3,7 @@ loadFramePacks([
 	{name:'Future Shifted', value:'FutureRegular'},
 	{name:'Seventh Edition', value:'Seventh'},
 	{name:'Fifth Edition', value:'SeventhButFifth'},
+	{name:'Fourth Edition', value:'Fourth'},
 	{name:'Legends Multicolored', value:'Legends'},
 	{name:'Alpha/Beta/Unlimited', value:'ABU'},
 	{name:'Misc', value:'disabled'},
