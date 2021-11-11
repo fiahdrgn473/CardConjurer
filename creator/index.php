9ffee0683b410c131b0499667c0aedc772fbdd7d
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2021-11-10 21:03:52 -0800

    Update index.php

--- creator/index.php
+++ creator/index.php
@@ -625,6 +625,7 @@ include('../globalHTML/header-1.php');
 				<h4>Britton N.</h4>
 				<h4>Westane</h4>
 				<h4>MoldyAce</h4>
+				<h4>Kerby</h4>
 			</div>
 			<h4 class='margin-bottom'>And of course, thank you to all of those who have made donations in the past.</h4>
 			<div class='supporters margin-bottom'>
