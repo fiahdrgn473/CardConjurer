34d96fb46291112b4c69cb21e85945cb10da9d5c
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2021-12-17 10:26:44 -0800

    Update packLegends.js

--- js/frames/packLegends.js
+++ js/frames/packLegends.js
@@ -26,13 +26,12 @@ document.querySelector('#loadFrameVersion').onclick = async function() {
 		title: {name:'Title', text:'', x:0.108, y:0.04, width:0.784, height:0.0405, oneLine:true, font:'goudymedieval', size:0.0405, color:'#dedede', shadowX:0.0027, shadowY:0.002},
 		type: {name:'Type', text:'', x:0.108, y:0.5524, width:0.784, height:0.0543, oneLine:true, size:0.032, color:'#dedede', shadowX:0.002, shadowY:0.0015},
 		rules: {name:'Rules Text', text:'', x:0.126, y:0.6081, width:0.748, height:0.2762, size:0.0362, manaPrefix:'old'},
-		pt: {name:'Power/Toughness', text:'', x:0.8, y:0.9039, width:0.1367, height:0.0453, size:0.0453, oneLine:true, align:'center', color:'#dedede', shadowX:0.0027, shadowY:0.002}
+		pt: {name:'Power/Toughness', text:'', x:0.8, y:0.91, width:0.1367, height:0.0453, size:0.0453, oneLine:true, align:'center', color:'#dedede', shadowX:0.0027, shadowY:0.002}
 	});
 	//bottom info
 	loadBottomInfo({
-		top: {text:'Illus: {elemidinfo-artist}', x:0.0614, y:0.902, width:0.8774, height:0.0281, oneLine:true, size:0.0281, align:'center', shadowX:0.0027, shadowY:0.002, color:'white'},
-		wizards: {name:'wizards', text:'\u2122 & \u00a9 ' + date.getFullYear() + ' Wizards of the Coast', x:0.0614, y:0.9315, width:0.8774, height:0.0172, oneLine:true, size:0.0172, align:'center', shadowX:0.0027, shadowY:0.002, color:'white'},
-		bottom: {text:'NOT FOR SALE   CardConjurer.com', x:0.0614, y:0.9505, width:0.8774, height:0.0143, oneLine:true, size:0.0143, align:'center', shadowX:0.0027, shadowY:0.002, color:'white'}
+		artist: {text:'Illus. \u00a9 {elemidinfo-artist}', x:0.1, y:0.92, width:0.7334, height:0.0277, oneLine:true, size:0.0277, align:'left', shadowX:0.0027, shadowY:0.002, color:'#dedede'},
+		cc: {text:'NOT FOR SALE   CardConjurer.com', x:0.0614, y:0.9791, width:0.8774, height:0.0124, oneLine:true, size:0.0124, align:'center', color:'#dedede', outlineWidth:0.003}
 	});
 }
 //loads available frames
