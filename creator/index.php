2bead75ab272c0143b19ce015affee34a5683815
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2021-12-21 10:58:30 -0800

    Update index.php

--- creator/index.php
+++ creator/index.php
@@ -655,6 +655,7 @@ include('../globalHTML/header-1.php');
 				<h4>Taya</h4>
 				<h4>Ayah N.</h4>
 				<h4>Aibis D.</h4>
+				<h4>Alex</h4>
 			</div>
 			<h4 class='margin-bottom'>And of course, thank you to all of those who have made donations in the past.</h4>
 			<div class='supporters margin-bottom'>
