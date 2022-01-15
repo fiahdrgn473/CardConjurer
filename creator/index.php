cfe8f3b40d99a8e4510cd35877650c2675306bf4
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2022-01-15 09:34:24 -0800

    Update index.php

--- creator/index.php
+++ creator/index.php
@@ -661,6 +661,7 @@ include('../globalHTML/header-1.php');
 				<h4>Pedro P.</h4>
 				<h4>Connor S.</h4>
 				<h4>Damian Z.</h4>
+				<h4>Adjicio</h4>
 			</div>
 			<h4 class='margin-bottom'>And of course, thank you to all of those who have made donations in the past.</h4>
 			<div class='supporters margin-bottom'>
