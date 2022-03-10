62f1f2598cca72c1a6841864eda31677a7a12d6c
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2022-03-10 08:32:13 -0800

    Update index.php

--- creator/index.php
+++ creator/index.php
@@ -710,6 +710,7 @@ include('../globalHTML/header-1.php');
 				<h4>Michael J.</h4>
 				<h4>Andrew K.</h4>
 				<h4>Wren</h4>
+				<h4>Camille D.</h4>
 			</div>
 			<h5 class='input-description'>(Please contact me if you would like your name/nickname changed.)</h5>
 		</div>
