d537e43238143379e000ff5bae0efbe98db3b68f
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2022-05-31 14:09:50 -0700

    Update index.php

--- creator/index.php
+++ creator/index.php
@@ -702,6 +702,7 @@ include('../globalHTML/header-1.php');
 				<h4>dave</h4>
 				<h4>Josh C.</h4>
 				<h4>Leo H.</h4>
+				<h4>Dawid S.</h4>
 			</div>
 			<h4 class='margin-bottom'>And of course, thank you to all of those who have made donations in the past.</h4>
 			<div class='supporters margin-bottom'>
