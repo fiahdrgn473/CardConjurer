16b8053e30b66de8976684876016859139ccff0d
Josh Birnholz <josh@birnholz.com>
2022-11-24 17:43:49 -0500

    Adjust ABU copyright

--- js/frames/packABU.js
+++ js/frames/packABU.js
@@ -48,7 +48,7 @@ document.querySelector('#loadFrameVersion').onclick = async function() {
 	});
 	//bottom info
 	loadBottomInfo({
-		artist: {text:'Illus. \u00a9 {elemidinfo-artist}', x:0.1, y:0.9, width:0.7334, height:0.0334, oneLine:true, size:0.0334, align:'left', shadowX:0.0027, shadowY:0.002, color:'#ababab'},
+		artist: {text:'Illus. {elemidinfo-artist}', x:0.1, y:0.9, width:0.7334, height:0.0334, oneLine:true, size:0.0334, align:'left', shadowX:0.0027, shadowY:0.002, color:'#ababab'},
 		cc: {text:'NOT FOR SALE   CardConjurer.com', x:0.0614, y:0.9791, width:0.8774, height:0.0124, oneLine:true, size:0.0124, align:'center', color:'#ababab', outlineWidth:0.003}
 	});
 }
