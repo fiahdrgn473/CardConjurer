e0c4158ee20814fb0ed66afd76997f0c1f093fa4
Josh Birnholz <josh@birnholz.com>
2022-12-18 03:39:33 -0500

    Add JumpStart 2022 Front Card frame

--- js/frames/packJ22Front.js
+++ js/frames/packJ22Front.js
@@ -1,6 +1,6 @@
 //defines available frames
 availableFrames = [
-	{name:'Frame', src:'/img/frames/token/jmpFront/frame.svg'}
+	{name:'Frame', src:'/img/frames/token/jmpFront/j22Frame.png'}
 ];
 //disables/enables the "Load Frame Version" button
 document.querySelector('#loadFrameVersion').disabled = false;
@@ -9,7 +9,7 @@ document.querySelector('#loadFrameVersion').onclick = async function() {
 	//resets things so that every frame doesn't have to
 	await resetCardIrregularities();
 	//sets card version
-	card.version = 'jmpFront';
+	card.version = 'j22Front';
 	//art bounds
 	card.artBounds = {x:0.0474, y:0.0353, width:0.9054, height:0.9296};
 	autoFitArt();
@@ -21,9 +21,9 @@ document.querySelector('#loadFrameVersion').onclick = async function() {
 	resetWatermark();
 	//text
 	loadTextOptions({
-		mana: {name:'Color Identity', text:'', y:0.9167, width:0.9292, height:83/2100, oneLine:true, size:83/1638, align:'right', manaCost:true, manaSpacing:0},
-		title: {name:'Title', text:'', x:0.0474, y:0.7039, width:0.9054, height:0.0534, oneLine:true, font:'gothammedium', size:0.0534, color:'white', align:'center'},
-		subtitle: {name:'Subtitle', text:'', x:0.0474, y:0.7543, width:0.9054, height:0.03, oneLine:true, font:'belerenbsc', size:0.03, color:'white', align:'center'}
+		mana: {name:'Color Identity', text:'', y:1752/2100, width:1, height:100/2100, oneLine:true, size:100/1638, align:'center', manaCost:true, manaSpacing:0},
+		title: {name:'Title', text:'', x:0.0474, y:1525/2100, width:0.9054, height:0.0534, oneLine:true, font:'gothammedium', size:0.0534, color:'white', align:'center'},
+		subtitle: {name:'Subtitle', text:'', x:0.0474, y:1631/2100, width:0.9054, height:0.03, oneLine:true, font:'belerenbsc', size:0.03, color:'white', align:'center'}
 	});
 	//bottom info
 	await loadBottomInfo({
@@ -35,5 +35,5 @@ document.querySelector('#loadFrameVersion').onclick = async function() {
 		wizards: {name:'wizards', text:'{ptshift0,0.0172}\u2122 & \u00a9 {elemidinfo-year} Wizards of the Coast. Illus. {elemidinfo-artist}', x:0.91, y:0.9762, width:1.3014, height:0.0153, oneLine:true, font:'mplantin', size:0.0153, color:'white', align:'right', rotation:270}
 	});
 }
-//loads available frames
+//loads available frame
 loadFramePack();
