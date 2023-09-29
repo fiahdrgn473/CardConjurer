317f0351be718a2f1cd5fb5c6e0cad7ea5e84b56
Josh birnholz <josh@birnholz.com>
2023-09-29 14:16:12 -0400

    Reorganize Universes Beyond section

---
+++ js/frames/groupUniversesBeyond.js
@@ -0,0 +1,22 @@
+loadFramePacks([	
+	{name:'Universes Beyond', value:'disabled'},
+	{name:'Universes Beyond', value:'UB'},
+	{name:'Universes Beyond (Full art)', value:'UBFull'},
+	{name:'Universes Beyond (Extended art)', value:'UBExtendedArt'},
+
+	{name:'Sagas (Universes Beyond)', value:'SagaUB'},
+
+	{name:'Addons', value:'disabled'},
+	{name:'Legend Crowns (Universes Beyond)', value:'UBLegendCrowns'},
+	{name:'Floating Legend Crowns (Universes Beyond)', value:'UBLegendCrownsFloating'},
+	{name:'Inner Crowns', value:'M15InnerCrowns'},
+	{name:'Nicknames', value:'M15Nickname-2'},
+	{name:'Holo Stamps', value:'M15HoloStamps'},
+	{name:'Dark Power/Toughness', value:'M15DarkPT'},
+	{name:'Colored Borders', value:'M15Borders'},
+	{name:'Color Identity Pips', value:'M15CIPips'},
+	{name:'"The List" Stamp', value:'TheList'},
+
+	{name:'Custom Addons', value:'disabled'},
+	{name:'Brawl Legend Crowns', value:'Brawl'}
+])
